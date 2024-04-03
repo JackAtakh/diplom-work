07c412cedd59c7c9d548cc6ab6dd6608987b24fd
JackAtakh <145584260+JackAtakh@users.noreply.github.com>
2024-04-03 17:08:25 +0500

    init

---
+++ main/java.js
@@ -0,0 +1,11 @@
+let b = document.getElementsByClassName('.white')[0];
+let opener = document.querySelector('.program')
+let closer = document.querySelector('.closeDescr')[0]
+
+
+function programDescr(){
+    opener.style.display='flex';
+}
+function closeProgram(){
+    opener.style.display='none';
+}
---
+++ main/script.js
@@ -0,0 +1,10 @@
+// start first screen js
+let a = document.getElementsByClassName('.biolent')[0];
+let clos = document.querySelector('.register')
+
+// a.addEventListener("click", sighUp())
+
+function sighUp(){
+    clos.style.display='flex';
+}
+// end first screen js
